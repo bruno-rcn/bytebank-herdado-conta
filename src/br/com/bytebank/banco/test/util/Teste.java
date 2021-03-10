95c91ff651712212e7e1c9726183c5d922b2b5fc
m240840 <bruno.noberto@sempreit.com.br>
2021-03-10 09:16:26 -0300

    pacote java util

--- src/br/com/bytebank/banco/test/util/Teste.java
+++ src/br/com/bytebank/banco/test/util/Teste.java
@@ -9,39 +9,23 @@ public class Teste {
 
 	public static void main(String[] args) {
 
-		ArrayList<Conta> lista = new ArrayList<Conta>();
+	    ArrayList<Conta> lista = new ArrayList<Conta>();
 
-		Conta cc = new ContaCorrente(22, 11);
-		lista.add(cc);
+	    Conta cc1 = new ContaCorrente(22, 22);
+	    Conta cc2 = new ContaCorrente(22, 22);
 
-		Conta cc2 = new ContaCorrente(22, 22);
-		lista.add(cc2);
+	    lista.add(cc1);
 
-		System.out.println("Tamanho: " + lista.size());
+	    boolean existe = lista.contains(cc2);
 
-		Conta ref = lista.get(0);
-		System.out.println(ref.getNumero());
+	    System.out.println("Já existe? " + existe);
 
-		lista.remove(0);
-		System.out.println("Tamanho: " + lista.size());
-
-		Conta cc3 = new ContaCorrente(33, 311);
-		lista.add(cc3);
-
-		Conta cc4 = new ContaCorrente(33, 322);
-		lista.add(cc4);
-
-		for (int i = 0; i < lista.size(); i++) {
-			Object oRef = lista.get(i);
-			System.out.println(oRef);
-		}
-
-		System.out.println("----------");
-
-		for (Conta oRef : lista) {
-			System.out.println(oRef);
-		}
-
+	    for(Conta conta : lista){
+	        System.out.println(conta);
+	    }
+	    
+	    boolean igual = cc1.equals(cc2);
+	    System.out.println(igual);
 	}
 
 }
